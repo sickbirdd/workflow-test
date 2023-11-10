07d6196d2d0ba8bd6d1c7e29c51e98fe4d152eae
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-11 01:31:55 +0900

    fix: pascalCase 수정

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,7 +3,7 @@ import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
-export class authService {
+export class AuthService {
     create(createAuthDto: CreateAuthDto) {
         return 'This action adds a new auth';
     }
