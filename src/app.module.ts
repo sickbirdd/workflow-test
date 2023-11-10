1c5b3eb2aacefdbe5893a25e8b04ed129b8e7131
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-11 01:23:48 +0900

    fix: prettier 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [UsersModule, AuthModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
